58a1f1987fc106c20138636486714cc61314d840
israel125x <30633437+israel125x@users.noreply.github.com>
2017-09-27 15:24:42 -0500

    sdfsd
    dsfdsf

--- server.js
+++ server.js
@@ -10,3 +10,26 @@ app.get('/*', function(req, res) {
 })
 
 console.log('Console listening!');
+
+const bodyParser = require('body-parser');
+const JSONbig = require('json-bigint');
+const path = require('path');
+const { Client } = require('pg');
+
+const client = new Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl: true,
+});
+
+client.connect();
+
+client.query('select now();', (err, res) => {
+    if (err) {
+        console.log(JSON.stringify(err));
+        throw err;
+    }
+    for (let row of res.rows) {
+        console.log(JSON.stringify(row));
+    }
+    client.end();
+});
