d88337c3466f4bbcdcab682f98a0278136b4ea87
israel125x <30633437+israel125x@users.noreply.github.com>
2017-09-29 11:40:47 -0500

    sdfds
    dsfsdf

--- src/app/registros/registro-list/registro-list.component.ts
+++ src/app/registros/registro-list/registro-list.component.ts
@@ -1,15 +1,15 @@
 //import { Component, OnInit } from '@angular/core';
 import { Component, OnInit } from '@angular/core';
-import { Registro } from '../registro';
-import { RegistroService } from '../registro.service';
-import { RegistroDetailsComponent } from '../registro-details/registro-details.component';
+//import { Registro } from '../registro';
+//import { RegistroService } from '../registro.service';
+//import { RegistroDetailsComponent } from '../registro-details/registro-details.component';
 
 @Component({
   //selector: 'app-registro-list',
   selector: 'registro-list',
   templateUrl: './registro-list.component.html',
   styleUrls: ['./registro-list.component.css'],
-  providers: [RegistroService]
+  //providers: [RegistroService]
 })
 export class RegistroListComponent implements OnInit {
 
