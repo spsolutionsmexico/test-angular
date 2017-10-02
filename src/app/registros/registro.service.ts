c8c5272e61980f6371eee09eb8eacc0f0e08d60c
israel125x <30633437+israel125x@users.noreply.github.com>
2017-10-02 11:23:59 -0500

    fgfdg
    fdgfdg

--- src/app/registros/registro.service.ts
+++ src/app/registros/registro.service.ts
@@ -1,5 +1,5 @@
 //import { Injectable } from '@angular/core';
-/*import { Injectable } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { Registro } from './registro';
 import { Http, Response } from '@angular/http';
 import 'rxjs/add/operator/toPromise';
@@ -9,25 +9,32 @@ export class RegistroService {
   private registrosUrl = '/api/registros';
 
   constructor(private http: Http) {
-    console.log('constructor(private');
+    console.log('constructor RegistroService ');
    }
   
-  getRegistros(): Promise<void | Registro[]> {
-    console.log('getRegistros start');
-    console.log('this.registrosUrl: ',this.registrosUrl);
-    return this.http.get(this.registrosUrl)
-               .toPromise()
-               .then(response => response.json() as Registro[])
-               .catch(this.handleError);
+  getRegistros(){
+    console.log('return getRegistros ');
+    return [
+      {
+          id : 123456,
+          nombre : "isra",
+          cp : "1001"
+      },
+      {
+          id : 7654321,
+          nombre : "usuario1",
+           cp: "2002"
+      }
+    ]
   }
-
+    
   private handleError (error: any) {
     let errMsg = (error.message) ? error.message :
     error.status ? `${error.status} - ${error.statusText}` : 'Server error';
     console.log('errMsg: ',errMsg); // log to console instead
   }
-}*/
-import { Injectable } from '@angular/core';
+}
+/*import { Injectable } from '@angular/core';
 @Injectable()
 export class RegistroService {
 constructor() { 
